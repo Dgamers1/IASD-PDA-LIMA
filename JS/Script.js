8046795bc362ac7ee6eb7a2c7bdfb73715ffbf43
Dgamers1 <daviggamers@gmail.com>
2026-08-01 10:21:06 -0300

    Add email validation for confirmation input

--- JS/Script.js
+++ JS/Script.js
@@ -1,1 +1,13 @@
+const email = document.getElementById("email");
+const confirmar = document.getElementById("confirmarEmail");
 
+function validarEmail() {
+    if (email.value !== confirmar.value) {
+        confirmar.setCustomValidity("Os e-mails não são iguais.");
+    } else {
+        confirmar.setCustomValidity("");
+    }
+}
+
+email.addEventListener("input", validarEmail);
+confirmar.addEventListener("input", validarEmail);
